835d5454fad20a3c6fe016e3c478a87f39e6a071
MohamedEidMohamed <mohamedeidfcih@gmail.com>
2024-04-05 16:29:09 +0200

    fix: all reoutes to dadd controllers dependency injection

--- routes/v1/buyers.php
+++ routes/v1/buyers.php
@@ -1,13 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\Buyer\BuyerController;
 
 Route::group([
-    'namespace' => 'Buyer',
-    'as' => 'buyers.',
+    // 'namespace' => 'Buyer',
+    // 'as' => 'buyers.',
     'middleware' => 'auth:sanctum',
 ], function () {
-    Route::post('/store-offer', 'BuyerController@store')->name('buyer.store');
-    Route::get('/getlist', 'BuyerController@getListForUser')->name('buyer.getList');
-    Route::get('/{id}', 'BuyerController@show')->name('buyer.show');
+    // Route::post('/store-offer', 'BuyerController@store')->name('buyer.store');
+    // Route::get('/getlist', 'BuyerController@getListForUser')->name('buyer.getList');
+    // Route::get('/{id}', 'BuyerController@show')->name('buyer.show');
+
+    Route::post('/store-offer',  [BuyerController::class, 'store'])->name('buyer.store');
+    Route::get('/getlist', [BuyerController::class, 'getListForUser'])->name('buyer.getList');
+    Route::get('/{id}', [BuyerController::class, 'show'])->name('buyer.show');
 });
--- routes/v1/categories.php
+++ routes/v1/categories.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\Category\CategoryController;
 
-Route::group(['namespace' => 'Category', 'as' => 'categories.'], function () {
-    Route::get('/', 'CategoryController@index')->name('list');
+Route::group([], function () { //'namespace' => 'Category', 'as' => 'categories.'
+    // Route::get('/', 'CategoryController@index')->name('list');
+    Route::get('/',  [CategoryController::class, 'index'])->name('list');
 });
