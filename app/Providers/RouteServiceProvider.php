355bf7b2756b852be5cda07987be83dcf8d0b038
MohamedEidMohamed <mohamedeidfcih@gmail.com>
2024-04-05 16:57:07 +0200

    trying handle cors

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,7 +29,7 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
+            Route::middleware(['api', 'cors'])
                 ->prefix('api')
                 ->namespace('\App\Http\Controllers\Api')
                 ->group(base_path('routes/api.php'));
