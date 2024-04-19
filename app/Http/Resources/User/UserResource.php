d3572d80d0fdef197fbb9106091b2d528c1a612e
MohamedEidMohamed <mohamedeidfcih@gmail.com>
2024-04-19 20:33:06 +0200

    fix: user update temporarily

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\User;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\Project\EmbededProjectResource;
 
 class UserResource extends JsonResource
 {
