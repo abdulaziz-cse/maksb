821ed90774d62f7b96dace81cd6991675521dab1
MohamedEidMohamed <mohamedeidfcih@gmail.com>
2024-05-29 22:04:19 +0300

    remove comments from auth route

--- routes/v2/auth.php
+++ routes/v2/auth.php
@@ -17,27 +17,3 @@ Route::group(['prefix' => 'v2/auth'], function () {
     Route::post('send-code', [VerificationController::class, 'sendOTP']);
     Route::post('verify-code',  [VerificationController::class, 'verifyOTP']);
 });
-
-// Route::group([], function () {
-//     Route::post('register', [AuthController::class, 'register']);
-
-//     Route::post('login', [AuthController::class, 'login']);
-
-    // Route::post('reset-password',  [AuthController::class, 'resetPassword']);
-
-    // Route::post('send-code', [VerificationController::class, 'sendCode']);
-
-    // Route::post('verify-code',  [VerificationController::class, 'verifyCode']);
-
-    // Route::middleware('auth:sanctum')->group(function () {
-    //     /**
-    //      * @group Authentication
-    //      * Get current user
-    //      */
-    //     Route::get('/user', function (\Illuminate\Http\Request $request) {
-    //         $user = $request->user();
-    //         $user->load('photo');
-    //         return response()->json($user);
-    //     })->name('user');
-    // });
-// });
