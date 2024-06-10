3797714da6dc930ac245f4167dd0aa368ed27978
MohamedEidMohamed <mohamedeidfcih@gmail.com>
2024-06-10 15:59:52 +0300

    handle cors with api's

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -27,7 +27,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::middleware(['api'])
+            Route::middleware(['api', 'cors'])
                 ->prefix('api')
                 ->namespace($this->namespace)
                 ->group([
